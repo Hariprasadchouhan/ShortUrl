b5e031cd0d2eba5516f08a7a7ed4bfac2baef2d4
rithwik <rithwik.varma@licious.com>
2024-11-21 23:01:01 +0530

    abc

--- src/main/java/com/example/Short_url/repository/ShortURLRepository.java
+++ src/main/java/com/example/Short_url/repository/ShortURLRepository.java
@@ -3,7 +3,6 @@ package com.example.Short_url.repository;
 import com.example.Short_url.model.ShortURL;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 
 
 public interface ShortURLRepository extends MongoRepository<ShortURL, String> {
